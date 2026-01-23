9972ba9dd308c08b8016e68518250fc778ee46bf
BinboLee <li_tuoyang@163.com>
2026-01-23 10:40:01 +0800

    只包含简单的HTTP请求

---
+++ src/main/java/com/example/SimpleHttp/SimpleHttpApplication.java
@@ -0,0 +1,14 @@
+package com.example.SimpleHttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+public class SimpleHttpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleHttpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SimpleHttp/SimpleHttpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SimpleHttp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleHttpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
